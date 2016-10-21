c4049cbbfcade442f9b62cb8366f955839583685
Grinch <grinchfedorov@gmail.com>
2016-10-21 03:08:14 +0300

    edit code conclusion global

--- krstcBot.py
+++ krstcBot.py
@@ -1,6 +1,7 @@
 __author__ = "Gregory Fedorov"
 __copyright__ = "Copyright 2016, All rights reserved."
-__email__ = "1.0.0"
+__version__ = "1.0.0"
+__mail__= "grinchfedorov@gmail.com"
 __status__ = "Production"
 # from logging import DEBUG
 import socket
@@ -20,7 +21,7 @@ bot = telebot.TeleBot(config.token)
 # telebot.logger.setLevel(DEBUG)
 
 
-txtHelp = """"
+txtHelp = """
 Команды:
 Меню - /menu
 Помощь - /help
@@ -48,7 +49,7 @@ def send_welcome(message):
     bot.send_message(message.chat.id,text='Привет! Я бот.', reply_markup=markup)
 
 @bot.message_handler(commands=["help"])
-def help(message):
+def send_help(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item_btn_menu = types.KeyboardButton('Меню')
     markup.row(item_btn_menu)
@@ -78,13 +79,13 @@ def settings(message):
 @bot.message_handler(func=lambda message: message.text == 'Расписание')
 def select_timetable(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item_btn_menu = types.KeyboardButton('Меню')
-    item_btn_12 = types.KeyboardButton('12');item_btn_16 = types.KeyboardButton('16')
-    item_btn_22 = types.KeyboardButton('22');item_btn_26_1 = types.KeyboardButton('26.1');item_btn_26_2 = types.KeyboardButton('26.2')
-    item_btn_32 = types.KeyboardButton('32');item_btn_36 = types.KeyboardButton('36');item_btn_37 = types.KeyboardButton('37')
     item_btn_42 = types.KeyboardButton('42');item_btn_46 = types.KeyboardButton('46');item_btn_47 = types.KeyboardButton('47')
+    item_btn_32 = types.KeyboardButton('32');item_btn_36 = types.KeyboardButton('36');item_btn_37 = types.KeyboardButton('37')
+    item_btn_22 = types.KeyboardButton('22');item_btn_26_1 = types.KeyboardButton('26.1');item_btn_26_2 = types.KeyboardButton('26.2')
+    item_btn_12 = types.KeyboardButton('12');item_btn_16 = types.KeyboardButton('16')
+    item_btn_menu = types.KeyboardButton('Меню')
     markup.row(item_btn_42, item_btn_46, item_btn_47)
-    markup.row(item_btn_32,item_btn_36,item_btn_37)
+    markup.row(item_btn_32, item_btn_36, item_btn_37)
     markup.row(item_btn_22, item_btn_26_1, item_btn_26_2)
     markup.row(item_btn_12, item_btn_16)
     markup.row(item_btn_menu)
@@ -109,9 +110,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=21, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=21, start_rowx=4, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 22, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "47", "/47"
 @bot.message_handler(commands=["47"])
@@ -122,13 +134,24 @@ def get_table(message):
 
     bot.send_message(message.chat.id, 'Загрузка файла с сервера 🔄')
     urlretrieve(config.url, config.file)
+
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=23, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
-
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=23, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 24, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 # Обработчик команд "42", "/42"
 @bot.message_handler(commands=["42"])
 @bot.message_handler(func=lambda message: message.text == '42')
@@ -141,9 +164,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=19, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=19, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 20, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "36", "/36"
 @bot.message_handler(commands=["36"])
@@ -157,9 +191,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True,formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx = 15,start_rowx = 4,end_rowx = 15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id,row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx = 15,start_rowx = 7,end_rowx = 15)
+    cabinet = open_sheet.col_values(colx = 16, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "37", "/37"
 @bot.message_handler(commands=["37"])
@@ -173,12 +218,22 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=17, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
-
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=17, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 18, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 # Обработчик команд "32", "/32"
-@bot.message_handler(commands=["32"])
+@bot.message_handler(commands = ["32"])
 @bot.message_handler(func=lambda message: message.text == '32')
 def get_table(message):
     timeout = 10
@@ -189,9 +244,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=13, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=13, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 14, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "12", "/12"
 @bot.message_handler(commands=["12"])
@@ -205,9 +271,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=3, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=3, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 4, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "16", "/16"
 @bot.message_handler(commands=["16"])
@@ -221,9 +298,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=5, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=5, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 6, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "22", "/22"
 @bot.message_handler(commands=["22"])
@@ -237,9 +325,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx=7, start_rowx=4, end_rowx=15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx=7, start_rowx=7, end_rowx=15)
+    cabinet = open_sheet.col_values(colx = 8, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "26.1", "/26_1"
 @bot.message_handler(commands=["26_1"])
@@ -253,9 +352,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand=True, formatting_info=True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx = 9, start_rowx = 4, end_rowx = 15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx = 9, start_rowx = 7, end_rowx = 15)
+    cabinet = open_sheet.col_values(colx = 10, start_rowx = 7, end_rowx = 15)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
 
 # Обработчик команд "26.2", "/26_2"
 @bot.message_handler(commands=["26_2"])
@@ -269,8 +379,20 @@ def get_table(message):
     open_xls = xlrd.open_workbook(config.source_xls, on_demand = True, formatting_info = True)
     open_sheet = open_xls.sheet_by_index(0)
     bot.send_message(message.chat.id, open_sheet.cell_value(0, 5))
-    rows = open_sheet.col_values(colx = 11, start_rowx = 4, end_rowx = 15)
-    row = '\n'.join(rows)
-    bot.send_message(message.chat.id, row)
+    day = (open_sheet.col_values(colx=1, start_rowx=7, end_rowx=20))
+    period = (open_sheet.col_values(colx=2, start_rowx=7, end_rowx=20))
+    subject = open_sheet.col_values(colx = 11, start_rowx = 7, end_rowx =20)
+    cabinet = open_sheet.col_values(colx=12, start_rowx=7, end_rowx=20)
+    cabinet = list(map(str,cabinet))
+    lessons = []
+    lessons.append(day)
+    lessons.append(period)
+    lessons.append(subject)
+    lessons.append(cabinet)
+    lessons = list(zip(*lessons))
+    for row in lessons:
+        if any(row):
+            bot.send_message(message.chat.id,' '.join(row))
+
 if __name__ == '__main__':
     bot.polling(none_stop = True)
